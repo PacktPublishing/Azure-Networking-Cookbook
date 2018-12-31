5faa35a1d963f44189495b83d7118b0e4cf65fd2
mustafatoroman <mustafatoroman@yahoo.com>
2018-12-31 12:41:06 +0100

    Delete CreateLocalNetworkGateway.ps1

--- Chapter 5/CreateLocalNetworkGateway.ps1
+++
@@ -1,1 +0,0 @@
-New-AzureRmLocalNetworkGateway -Name packt-lng-script -ResourceGroupName 'Packt-Networking-Script' -Location 'westeurope' -GatewayIpAddress '195.222.10.20' -AddressPrefix '192.168.1.0/24'
