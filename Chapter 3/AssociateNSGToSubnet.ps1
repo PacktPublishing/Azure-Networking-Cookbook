ed09a50f68a11451f79306b9fee6c90066353245
swathym14 <32697994+swathym14@users.noreply.github.com>
2019-03-29 11:26:31 +0530

    Code Files Added

--- Chapter 3/AssociateNSGToSubnet.ps1
+++
@@ -1,6 +0,0 @@
-﻿
-$vnet = Get-AzureRmVirtualNetwork -Name 'Packt-Script' -ResourceGroupName 'Packt-Networking-Script'
-$subnet = Get-AzureRmVirtualNetworkSubnetConfig -VirtualNetwork $vnet -Name BackEnd
-$nsg = Get-AzureRmNetworkSecurityGroup -ResourceGroupName 'Packt-Networking-Script' -Name 'nsg1'
-$subnet.NetworkSecurityGroup = $nsg
-Set-AzureRmVirtualNetwork -VirtualNetwork $vnet
---
+++ Chapter01/CreateSubnet.ps1
@@ -0,0 +1,5 @@
+﻿$VirtualNetwork = Get-AzureRmVirtualNetwork -Name 'Packt-Script' -ResourceGroupName 'Packt-Networking-Script' 
+
+Add-AzureRmVirtualNetworkSubnetConfig -Name FrontEnd -AddressPrefix 10.11.0.0/24 -VirtualNetwork $virtualNetwork
+
+$virtualNetwork | Set-AzureRmVirtualNetwork
